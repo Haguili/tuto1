ce9350384ccc2532797bee3b80f1a7410e302ccc
hassen aguili <hassen.aguili@ensta-paris.fr>
2020-12-23 17:11:15 +0100

    commit_21_12_17_11

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,68 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+
 class Square extends React.Component {
-    constructor(props){
-      super(props); 
-      // we need props for the value
-
-      /* In JavaScript classes, you need to always call super when defining the constructor
-       of a subclass. All React component classes that have a constructor should start with a
-      super(props) call.
-      */
-
-      this.state={value:null,};
-
-      // value X or not 
-
-
-    }
-
-
     render() {
       return (
-        // the square is actually a button
-        <button className="square" onClick={() => this.setState({value: 'X'})} >
-          {//this.props.value but we change it to state
-          this.state.value
-          
-          }
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
-  /* To save typing and avoid the confusing behavior of this, 
-  we will use the arrow function syntax for event handlers 
-  so  we replaced : onClick={function() {alert('click');}}
-  with onclick{function() => alert('click') }
-  */
-
-  /* Notice how with onClick={() => alert('click')},
-   we’re passing a function as the onClick prop. 
-   React will only call this function after a click. 
-   Forgetting () => and writing onClick={alert('click')} is a common mistake, 
-   and would fire the alert every time the component re-renders.
-
-  */
-
-  /* onClick={() => this.setState({value: 'X'})}
-Square’s this.state.value will be 'X', so we’ll see the X on the game board
-
-
-  */
   
   class Board extends React.Component {
-    constructor(props){
+    constructor(props) {
       super(props);
-      this.state = { squares : Array(9).fill(null),};
-
-
+      this.state = {
+        squares: Array(9).fill(null),
+      };
     }
-
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+  
     renderSquare(i) {
-      // return <Square value={i} />;
-      return <Square value={this.state.squares[i]} /> ;
-
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
@@ -74,19 +47,13 @@ Square’s this.state.value will be 'X', so we’ll see the X on the game board
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
           </div>
         </div>
       );
