ffea3b430d38fe1efc372e312e21ea840bc8b193
hassen aguili <hassen.aguili@ensta-paris.fr>
2020-12-23 13:45:20 +0100

    tutos_21_12_13_45

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,24 +5,66 @@ import './index.css';
 
 class Square extends React.Component {
     constructor(props){
-      super(props);
+      super(props); 
+      // we need props for the value
+
+      /* In JavaScript classes, you need to always call super when defining the constructor
+       of a subclass. All React component classes that have a constructor should start with a
+      super(props) call.
+      */
+
       this.state={value:null,};
+
+      // value X or not 
 
 
     }
+
+
     render() {
       return (
-        <button className="square" onClick={function() {alert('click');}} >
-          {this.props.value}
+        // the square is actually a button
+        <button className="square" onClick={() => this.setState({value: 'X'})} >
+          {//this.props.value but we change it to state
+          this.state.value
+          
+          }
         </button>
       );
     }
   }
-  
+  /* To save typing and avoid the confusing behavior of this, 
+  we will use the arrow function syntax for event handlers 
+  so  we replaced : onClick={function() {alert('click');}}
+  with onclick{function() => alert('click') }
+  */
+
+  /* Notice how with onClick={() => alert('click')},
+   we’re passing a function as the onClick prop. 
+   React will only call this function after a click. 
+   Forgetting () => and writing onClick={alert('click')} is a common mistake, 
+   and would fire the alert every time the component re-renders.
+
+  */
+
+  /* onClick={() => this.setState({value: 'X'})}
+Square’s this.state.value will be 'X', so we’ll see the X on the game board
+
+
+  */
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = { squares : Array(9).fill(null),};
+
+
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      // return <Square value={i} />;
+      return <Square value={this.state.squares[i]} /> ;
+
     }
   
     render() {
