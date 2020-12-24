9f96d392c29d279761ba095d8a1bcdd99ae71777
hassen aguili <hassen.aguili@ensta-paris.fr>
2020-12-24 16:53:03 +0100

    push this

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -121,6 +121,27 @@ function Square(props) {
         const history = this.state.history;
         const current = history[history.length -1];
         const winner = calculateWinner(current.squares);
+        const moves = history.map((step,move) => 
+        
+        {
+
+
+          const desc = move ? 'Go to move # ' + move : 'go to game start ' ;
+
+          return  ( 
+
+            <li> <button  onClick={() => this.jumpTo(move)} > {desc}      </button></li>
+
+
+          )
+
+
+
+        }
+        );
+
+
+
         let status;
         if ( winner)
         {status = 'winner:'+winner;} else { status = 'the next player is:' + (this.state.xIsNext ? 'X' : 'O')}
@@ -138,7 +159,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
